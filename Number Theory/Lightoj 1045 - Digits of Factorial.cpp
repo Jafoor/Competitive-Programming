dcaa20240ecc93ba5be0b1873c0e0f2e73207529
Abu Jafor Mohammad Saleh <37769549+Jafoor@users.noreply.github.com>
2020-01-21 12:30:21 +0600

    Lightoj Nuber Theory 1045 - Digits of Factorial

---
+++ Number Theory/Lightoj 1045 - Digits of Factorial.cpp
@@ -0,0 +1,77 @@
+/*
+ Abu Jafor Mohammad Saleh
+ ios_base :: sync_with_stdio(false);
+ cin.tie(NULL);
+ */
+
+#include<cstdio>
+#include<iomanip>
+#include<sstream>
+#include<cstdlib>
+#include<cctype>
+#include<cmath>
+#include<algorithm>
+#include<set>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<list>
+#include<iostream>
+#include<fstream>
+#include<numeric>
+#include<string>
+#include<vector>
+#include<cstring>
+#include<map>
+#include<iterator>
+#include<limits>
+#include <algorithm>
+#include <unordered_map>
+#include<fstream>
+#define f first
+#define s second
+#define MOD 1000000007
+
+#define INF (int)1e9
+#define PI 3.1415926535897932384626433832795
+#define BOUNDARY(i, j) ((i >= 0 && i < rows) && (j >= 0 && j < coloums))
+#define mx 6e5 + 100;
+#define fast ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+//#include<bits/stdc++.h>
+#define ll long long
+const int maxn = 20005;
+using namespace std;
+int X[] = {0, 1, 0, -1};
+int Y[] = {-1, 0, 1, 0};
+int rows,coloums;
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+double a[1000000+1];
+void solve()
+{
+    a[0] = 0;
+    for(int i=1;i<=1000000;i++)
+    {
+        a[i] = a[i-1] + log10(i);
+    }
+}
+
+int main()
+{
+    fast;
+    int T;
+    solve();
+    cin>>T;
+    for(int t=1; t<=T; t++)
+    {
+        int n,m;
+        cin>>n>>m;
+        int ans = (a[n] / log10(m)) + 1;
+        cout<<"Case "<<t<<": "<<ans<<endl;
+    }
+}
